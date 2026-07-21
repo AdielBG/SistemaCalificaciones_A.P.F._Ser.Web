d5547b13002ebe3e18a5e54118c00dc49d8f04d0
AdielBG <adielbg17.2@gmail.com>
2026-07-21 11:37:22 -0400

    ProgramaAcademico Model

---
+++ SistemaCalificaciones/Models/ProgramaAcademico.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCalificaciones.Models
+{
+    public class ProgramaAcademico
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del programa es requerido.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres.")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [StringLength(250, ErrorMessage = "La descripción no puede superar 250 caracteres.")]
+        public string Descripcion { get; set; } = string.Empty;
+    }
+}
