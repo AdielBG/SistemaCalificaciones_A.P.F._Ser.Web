5c404eb97e8c9cab58d02320ab8ea61a62229008
AdielBG <adielbg17.2@gmail.com>
2026-07-21 11:43:27 -0400

    Materia model

---
+++ SistemaCalificaciones/Models/Materia.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCalificaciones.Models
+{
+    public class Materia
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la materia es requerido.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres.")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El código de la materia es requerido.")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "El código debe tener entre 2 y 20 caracteres.")]
+        public string Codigo { get; set; } = string.Empty;
+
+        [Range(1, 10, ErrorMessage = "Los créditos deben estar entre 1 y 10.")]
+        public int Creditos { get; set; }
+
+        // Relación con Profesor
+        public int ProfesorId { get; set; }
+    }
+}
