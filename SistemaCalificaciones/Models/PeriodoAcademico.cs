d49864a42f8c1b3e8acd6bbaca767ecf338a70af
AdielBG <adielbg17.2@gmail.com>
2026-07-21 11:38:33 -0400

    PeriodoAcademico model

---
+++ SistemaCalificaciones/Models/PeriodoAcademico.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCalificaciones.Models
+{
+    public class PeriodoAcademico
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del período es requerido.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 50 caracteres.")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La fecha de inicio es requerida.")]
+        public DateTime FechaInicio { get; set; }
+
+        [Required(ErrorMessage = "La fecha de fin es requerida.")]
+        public DateTime FechaFin { get; set; }
+
+        public bool Activo { get; set; } = true;
+    }
+}
