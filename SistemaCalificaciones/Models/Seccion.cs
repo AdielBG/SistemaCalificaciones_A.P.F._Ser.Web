42e3699fc2df4ae6219a588d272e762ff938f8c8
AdielBG <adielbg17.2@gmail.com>
2026-07-21 11:42:30 -0400

    Seccion model

---
+++ SistemaCalificaciones/Models/Seccion.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCalificaciones.Models
+{
+    public class Seccion
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la sección es requerido.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "El nombre debe tener entre 1 y 50 caracteres.")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Range(1, 200, ErrorMessage = "La capacidad debe estar entre 1 y 200 estudiantes.")]
+        public int Capacidad { get; set; }
+    }
+}
